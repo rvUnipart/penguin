f53a9e53d9bff11e3fecad55bc24a37525e206fd
Robin Vize <69241122+rvUnipart@users.noreply.github.com>
2021-03-26 13:50:02 +0000

    Update app.ts
    removed the bottom section

--- app.ts
+++ app.ts
@@ -4,15 +4,3 @@ import * as flags from "https://deno.land/std@0.83.0/flags/mod.ts";
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
-
-if (isNaN(port)) {
-  console.error("Port is not a number.");
-  Deno.exit(1);
-}
-
-const s = serve({ port: port });
-console.log("http://localhost:" + port);
-
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}
