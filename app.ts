3c7d1d6adaaaeb2f2943d8adda42b97217627271
Robin Vize <69241122+rvUnipart@users.noreply.github.com>
2021-03-26 13:56:02 +0000

    deleted app.js

--- app.ts
+++
@@ -1,6 +0,0 @@
-import { serve } from "https://deno.land/std@0.83.0/http/server.ts";
-import * as flags from "https://deno.land/std@0.83.0/flags/mod.ts";
-
-const DEFAULT_PORT = 8080;
-const argPort = flags.parse(Deno.args).port;
-const port = argPort ? Number(argPort) : DEFAULT_PORT;
